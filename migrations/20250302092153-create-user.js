9a4de40c6790fae7ad3c2e0b741d7d1c68266ec0
YellowLikeSun <123458383+YellowLikeSun@users.noreply.github.com>
2025-03-02 13:26:42 +0100

    il veut commit
    g

--- migrations/20250302092153-create-user.js
+++ migrations/20250302092153-create-user.js
@@ -1,31 +1,46 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Users", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       nom: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      prenom: {
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      pseudo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
-  }
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Users");
+  },
 };
--- src/routes/registerRoutes.js
+++ src/routes/registerRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { registerUser } = require("../controllers/registerController");
+const { registerUser } = require("../controllers/RegisterController"); // Import du contrôleur
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+// Route d'inscription
+router.post("/", registerUser);
 
 module.exports = router;
