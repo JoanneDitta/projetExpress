a4585c8d5a6157e801ab6fe8838fbe0f2b1a2a22
JoanneDitta <joanne.ditta@etu.univ-tours.fr>
2025-03-02 10:28:51 +0100

    update hey --> test

--- src/routes/heyRoutes.js
+++ src/routes/heyRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const heyController = require('../controllers/heyController');
 
-router.get('/hey', heyController.getHey);
+router.get('/test', heyController.getHey);
 
 module.exports = router;
