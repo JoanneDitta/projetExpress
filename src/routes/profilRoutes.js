b30723ec8baf623c237a53e061ef8499c595bcec
JoanneDitta <joanne.ditta@etu.univ-tours.fr>
2025-03-02 17:15:04 +0100

    update profile a la main

--- src/routes/profilRoutes.js
+++ src/routes/profilRoutes.js
@@ -5,6 +5,6 @@ const { getUserProfile } = require("../controllers/profilController");
 const router = express.Router();
 
 // Route GET pour récupérer le profil utilisateur (nécessite un token)
-router.get("/", authenticateToken, getUserProfile);
+router.get("/:id", authenticateToken, getUserProfile);
 
 module.exports = router;
